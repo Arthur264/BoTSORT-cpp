bb7fc7cebc77d18d6042485fc5bbfa5d5b684c3b
Nuzhny007 <nuzhny@mail.ru>
2025-11-13 22:59:40 +0300

    Fix types

--- botsort/src/utils.cpp
+++ botsort/src/utils.cpp
@@ -8,12 +8,12 @@ double lapjv(CostMatrix &cost, std::vector<int> &rowsol,
              std::vector<int> &colsol, bool extend_cost, float cost_limit,
              bool return_cost)
 {
-    int n_rows = static_cast<int>(cost.rows());
-    int n_cols = static_cast<int>(cost.cols());
+    size_t n_rows = cost.rows();
+    size_t n_cols = cost.cols();
     rowsol.resize(n_rows);
     colsol.resize(n_cols);
 
-    int n = 0;
+    size_t n = 0;
     if (n_rows == n_cols)
     {
         n = n_rows;
@@ -32,14 +32,14 @@ double lapjv(CostMatrix &cost, std::vector<int> &rowsol,
     {
         n = n_rows + n_cols;
         cost_c.resize(n);
-        for (int i = 0; i < cost_c.size(); i++)
+        for (size_t i = 0; i < cost_c.size(); i++)
             cost_c[i].resize(n);
 
         if (cost_limit < LONG_MAX)
         {
-            for (int i = 0; i < cost_c.size(); i++)
+            for (size_t i = 0; i < cost_c.size(); i++)
             {
-                for (int j = 0; j < cost_c[i].size(); j++)
+                for (size_t j = 0; j < cost_c[i].size(); j++)
                 {
                     cost_c[i][j] = cost_limit / 2.0;
                 }
@@ -56,18 +56,18 @@ double lapjv(CostMatrix &cost, std::vector<int> &rowsol,
                         cost_max = cost(i, j);
                 }
             }
-            for (int i = 0; i < cost_c.size(); i++)
+            for (size_t i = 0; i < cost_c.size(); i++)
             {
-                for (int j = 0; j < cost_c[i].size(); j++)
+                for (size_t j = 0; j < cost_c[i].size(); j++)
                 {
                     cost_c[i][j] = cost_max + 1;
                 }
             }
         }
 
-        for (int i = n_rows; i < cost_c.size(); i++)
+        for (size_t i = n_rows; i < cost_c.size(); i++)
         {
-            for (int j = n_cols; j < cost_c[i].size(); j++)
+            for (size_t j = n_cols; j < cost_c[i].size(); j++)
             {
                 cost_c[i][j] = 0;
             }
@@ -108,25 +108,30 @@ double lapjv(CostMatrix &cost, std::vector<int> &rowsol,
 
     if (n != n_rows)
     {
-        for (int i = 0; i < n; i++)
+        for (size_t i = 0; i < n; i++)
         {
-            if (x_c[i] >= n_cols) x_c[i] = -1;
-            if (y_c[i] >= n_rows) y_c[i] = -1;
+            if (x_c[i] >= n_cols)
+                x_c[i] = -1;
+            if (y_c[i] >= n_rows)
+                y_c[i] = -1;
         }
-        for (int i = 0; i < n_rows; i++) { rowsol[i] = x_c[i]; }
-        for (int i = 0; i < n_cols; i++) { colsol[i] = y_c[i]; }
+        for (size_t i = 0; i < n_rows; i++)
+            rowsol[i] = x_c[i];
+        for (size_t i = 0; i < n_cols; i++)
+            colsol[i] = y_c[i];
 
         if (return_cost)
         {
-            for (int i = 0; i < rowsol.size(); i++)
+            for (size_t i = 0; i < rowsol.size(); i++)
             {
-                if (rowsol[i] != -1) { opt += cost_c[i][rowsol[i]]; }
+                if (rowsol[i] != -1)
+                    opt += cost_c[i][rowsol[i]];
             }
         }
     }
     else if (return_cost)
     {
-        for (int i = 0; i < rowsol.size(); i++)
+        for (size_t i = 0; i < rowsol.size(); i++)
         {
             opt += cost_c[i][rowsol[i]];
         }
